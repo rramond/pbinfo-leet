722a4c4717c3c2d393817235395dcb9e8fcb23d8
rramond <raresdunca17@gmail.com>
2026-03-23 00:14:47 +0200

    o problema de nota 10

---
+++ Paradigms/Backtracking/permutariPF.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Se citește un număr natural n (n<16). Afișați în ordine lexicografică toate permutările mulțimii {1,2,…,n} în care elementele pare sunt puncte fixe (nu își schimbă poziția).
+
+int n;
+vector<int> perm; //permutarea finala cu elementele pare deja plasate
+vector<int> odds;  //vector cu elementele impare care trebuie permutate {1, 3, 5, ...}
+vector<bool> used;
+
+void afis() {
+    for (int x : perm)
+        cout << x << ' ';
+    cout << '\n';
+}
+
+//slot reprezinta pozitia curenta in care trebuie sa punem un element impar = ce pozitie para umplem acm (0, 2, 4, ...)
+void BKT(int slot) {
+    if (slot >= n) { 
+        afis();
+        return;
+    }
+    for (int i = 0; i < (int)odds.size(); i++) {
+        if (!used[i]) {
+            used[i] = true;
+            perm[slot] = odds[i]; //bagam elementul impar in slotul curent
+            BKT(slot + 2);
+            used[i] = false;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+    perm.resize(n);
+
+for (int i = 1; i < n; i += 2)
+    perm[i] = i + 1; 
+
+for (int i = 0; i < n; i += 2)
+    odds.push_back(i + 1); 
+
+    used.assign(odds.size(), false);
+
+    BKT(0); 
+
+    return 0;
+}
